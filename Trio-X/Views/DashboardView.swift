11c7a0e72aad3dc27ff92442beec94295048f96d
Kunal <kunal17058@gmail.com>
2024-09-28 00:23:12 +0530

    Add Dashboard Screen

---
+++ Trio-X/Views/DashboardView.swift
@@ -0,0 +1,37 @@
+//
+//  DashboardView.swift
+//  Trio-X
+//
+//  Created by Kunal Yelne on 27/09/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                NavigationLink(destination: ContentView()) {
+                    Label("Process Monitoring", systemImage: "gearshape")
+                }
+                NavigationLink(destination: ContentView()) {
+                    Label("File Integrity Check", systemImage: "folder")
+                }
+                NavigationLink(destination: ContentView()) {
+                    Label("Security Privileges", systemImage: "shield")
+                }
+            }
+            .listStyle(SidebarListStyle())
+            .navigationTitle("Trio-X")
+            
+            Text("Select a feature")
+                .font(.largeTitle)
+                .foregroundColor(.secondary)
+        }
+    }
+}
+
+#Preview {
+    DashboardView()
+}
